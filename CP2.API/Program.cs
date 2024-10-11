c6e50eaa967338c87badb48b33a1709192bc82a2
diogo_gallina <diogogallina20@gmail.com>
2024-10-11 01:24:55 -0300

    feat

---
+++ CP2.API/Program.cs
@@ -0,0 +1,37 @@
+using CP2.Data.AppData;
+using CP2.IoC; 
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseOracle(builder.Configuration["ConnectionStrings:DefaultConnection"],
+        b => b.MigrationsAssembly("CP2.API")); 
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+
+Bootstrap.Start(builder.Services, builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
